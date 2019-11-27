921b052cf95752d202438f6c6f1c68cd5833d914
aso1901102 <1901102@s.asojuku.ac.jp>
2019-11-27 11:37:28 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/asojuku/animalbook/SubActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.asojuku.animalbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
