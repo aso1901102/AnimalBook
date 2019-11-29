08cbc6596aa6102f4e5e7973f601279aa744ad36
aso1901102 <1901102@s.asojuku.ac.jp>
2019-11-29 12:28:52 +0900

    ver2.2

--- app/src/main/java/jp/ac/asojuku/animalbook/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/animalbook/MainActivity.kt
@@ -1,5 +1,6 @@
 package jp.ac.asojuku.animalbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,5 +20,17 @@ class MainActivity : AppCompatActivity() {
         //画面のフラグメントをIDで取得してオリジナルのフラグメントクラスにキャストして定数に代入
         var fragment = this.titleFragment as? TitleFragment
         fragment?.setTitle("フラグメント動物図鑑");
+
+        //「次へ」ボタンがクリックされた時のコールバックメソッド
+        this.nextButton.setOnClickListener {
+            //ボタンがクリックされた時の処理
+            //画面遷移用の院展とを生成
+            val intent = Intent(this,SubActivity::class.java)
+            //intentをもとに画面遷移を実行
+            this.startActivity(intent)
+
+        }
+
+
     }
 }
