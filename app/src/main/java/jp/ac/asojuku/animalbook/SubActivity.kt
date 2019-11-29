cfd67a508c553f9ee14771cee91566b628d1dbbe
aso1901102 <1901102@s.asojuku.ac.jp>
2019-11-29 12:15:28 +0900

    ver2.1

--- app/src/main/java/jp/ac/asojuku/animalbook/SubActivity.kt
+++ app/src/main/java/jp/ac/asojuku/animalbook/SubActivity.kt
@@ -2,11 +2,73 @@ package jp.ac.asojuku.animalbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+    }
+
+    //画面の表示・再表示のイベントコールバックメソッド
+    override fun onResume() {
+        super.onResume()
+        //ライオンボタンがクリックされたときのコールバックメソッドのリスナーを設定
+        this.lionButton.setOnClickListener {
+            //クリックされたときの処理
+            //ライオンフラグメントのインスタンスを生成
+            val fragment = LionFragment()
+
+            //画面を操作できるフラグメントマネージャーを取得
+            //変数に代入
+            val fragmentManager = this.supportFragmentManager
+
+            //フラグメント切り替えのためにフラグメント操作のトランザクションを開始
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            //トランザクションが開始されたのでフラグメントんい関する操作をする
+            fragmentTransaction.replace(R.id.container,fragment)       //画面のid:container部分にフラグメントを保存
+                .addToBackStack(null)                           //元のフラグメントをバックスタックに保存
+                .commit()                                              //トランザクションの完了
+        }
+
+        //カバのボタンがクリックされたときのイベントのコールバックメソッド
+        this.hippoButton.setOnClickListener {
+            //クリックされたときの処理
+            //かばフラグメントのインスタンスを生成
+            val fragment = HippoFragment()
+
+            //画面を操作できるフラグメントマネージャーを取得
+            //変数に代入
+            val fragmentManager = this.supportFragmentManager
+
+            //フラグメント切り替えのためにフラグメント操作のトランザクションを開始
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            //トランザクションが開始されたのでフラグメントに関する操作をする
+            fragmentTransaction.replace(R.id.container,fragment)       //画面のid:container部分にフラグメントを保存
+                .addToBackStack(null)                           //元のフラグメントをバックスタックに保存
+                .commit()                                              //トランザクションの完了
+        }
+
+        //キリンのボタンがクリックされたときのイベントのコールバックメソッド
+        this.giraffeButton.setOnClickListener {
+            //クリックされたときの処理
+            //かばフラグメントのインスタンスを生成
+            val fragment = giraffeFragment()
+
+            //画面を操作できるフラグメントマネージャーを取得
+            //変数に代入
+            val fragmentManager = this.supportFragmentManager
+
+            //フラグメント切り替えのためにフラグメント操作のトランザクションを開始
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            //トランザクションが開始されたのでフラグメントに関する操作をする
+            fragmentTransaction.replace(R.id.container,fragment)       //画面のid:container部分にフラグメントを保存
+                .addToBackStack(null)                           //元のフラグメントをバックスタックに保存
+                .commit()                                              //トランザクションの完了
+        }
     }
 }
